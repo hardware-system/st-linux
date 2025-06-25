df87c3934777e8349f821685c22fd5b3e06af164
liuxin <1242129679@qq.com>
2025-06-25 14:12:31 +0800

    Update kernel version banner to reflect custom version
    - Modify the Linux proc banner to display "ici-c2-kernel_v3.0.1" as the version string

--- init/version.c
+++ init/version.c
@@ -33,7 +33,7 @@ static int __init early_hostname(char *arg)
 early_param("hostname", early_hostname);
 
 const char linux_proc_banner[] =
-	"%s version %s"
+	"ici-c2-kernel_v3.0.1 %s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
 	" (" LINUX_COMPILER ") %s\n";
 
